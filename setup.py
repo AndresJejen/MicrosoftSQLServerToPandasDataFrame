f1f1581cf87d23a3c647fd019a1df38317e56991
AndresJejen <gajcam@gmail.com>
2020-07-07 12:44:48 -0500

    [Feat] Set the Push to PIP

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="SQLServerToPandasDataFrame-YOUR-USERNAME-HERE", # Replace with your own username
+    name="SQLServerToPandasDataFrame-germanandresjejencortes", # Replace with your own username
     version="0.0.1",
     author="Andres Jejen",
     author_email="gajcam@gmail.com",
