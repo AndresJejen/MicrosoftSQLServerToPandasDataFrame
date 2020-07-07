fdd5ac799331c3d4e726334ac21f9a7f5dcd8877
AndresJejen <gajcam@gmail.com>
2020-07-07 09:10:50 -0500

    [Feat] package project structure

--- SQLServerToPandasDataFrame/__init__.py
+++ SQLServerToPandasDataFrame/__init__.py
@@ -1,5 +1,9 @@
 from os import listdir
 from os.path import isfile, isdir
+from pyodbc import Connection
+import pyodbc
+import pandas.io.sql
+from pandas import DataFrame
 from .constants import BaseSQLQuery
 
 def CreateObjectEmpresa(empresa: str, DataBase: str, Table: str) -> None:
@@ -18,12 +22,12 @@ def CreateObjectEmpresa(empresa: str, DataBase: str, Table: str) -> None:
     return empresaObject
 
 def FilesToCompanyDict(path: str) -> None:
-    empresas = {}
     """[Convert a list of sql files to a Dict objects]
 
     Args:
         path (str): [description]
     """
+    empresas = {}
     files = QueryFilesNames(path)
     for file in files:
         f=open(path+file, "r")
@@ -46,12 +50,11 @@ def QueryFilesNames(path: str) -> list:
     """
     return [obj for obj in listdir(path) if isfile(path + obj)]
 
-def CreateQuery(company: dict, test: bool = True) -> str:
+def CreateQuery(company: dict, BaseQuery: str, test: bool = True) -> str:
     """[Create a SQL QUERY based on the company data]
 
     Args:
         company (dict): [Dictionay with the data]
-        test (bool, optional): [Query first 10 lines]. Defaults to false.
 
     Returns:
         str: [SQL QUERY for ]
@@ -61,3 +64,30 @@ def CreateQuery(company: dict, test: bool = True) -> str:
     Table = company["Table"]
     limit = "TOP 10" if test == True else ""
     return BaseSQLQuery.format(limit,Empresa, DataBase, Table)
+
+def runQuery(sql: str, connection: Connection) -> DataFrame:
+    """Query all the data to the DataBase Server
+
+    Args:
+        sql (str): SQL query string 
+        connection (Connection): Connection Object
+
+    Returns:
+        DataFrame: Pandas dataframe with data
+    """
+    return pandas.io.sql.read_sql(sql, connection)
+
+def createConnection(databaseName: str, server: str,user: str, password: str, driver: str ="ODBC Driver 17 for SQL Server") -> Connection:
+    """Creates a Conection to a SQL SERVER Database
+
+    Args:
+        databaseName (str): Name of the database
+        server (str): Database Server IP or hostname
+        user (str): user Name
+        password (str): Connection credentials
+        driver (str, optional): Connection Driver. Defaults to "ODBC Driver 17 for SQL Server".
+
+    Returns:
+        Connection: a connection object
+    """
+    return pyodbc.connect("DRIVER={"+driver+"};SERVER="+server+";DATABASE="+databaseName+";UID="+user+";PWD="+password)
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="SQLServerToPandasDataFrame-YOUR-USERNAME-HERE", # Replace with your own username
+    version="0.0.1",
+    author="Andres Jejen",
+    author_email="gajcam@gmail.com",
+    description="Query Data from a Private DataBase and save it in a Pandas DataFrame",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
